e483b70f63960607f01cfdc8f475c32d85bb19ce
ramdev222 <ramdevcalope1995@gmail.com>
2020-12-18 14:38:04 +0800

    camera

---
+++ src/hooks/usePhotoGallery.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import { useCamera } from '@ionic/react-hooks/camera';
+import { useFilesystem, base64FromPath } from '@ionic/react-hooks/filesystem';
+import { useStorage } from '@ionic/react-hooks/storage';
+import { isPlatform } from '@ionic/react';
+import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from "@capacitor/core";
+
+
+export function usePhotoGallery() {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const { getPhoto } = useCamera();
+
+
+  const takePhoto = async () => {
+  const cameraPhoto = await getPhoto({
+    resultType: CameraResultType.Uri,
+    source: CameraSource.Camera,
+    quality: 100
+  });
+
+  const fileName = new Date().getTime() + '.jpeg';
+  const savedFileImage = await savePicture(cameraPhoto, fileName);
+  const newPhotos = [savedFileImage, ...photos];
+  setPhotos(newPhotos);
+};
+
+  const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
+
+  const savePicture = async (photo: CameraPhoto, fileName: string): Promise<Photo> => {
+  const base64Data = await base64FromPath(photo.webPath!);
+  const savedFile = await writeFile({
+    path: fileName,
+    data: base64Data,
+    directory: FilesystemDirectory.Data
+  });
+
+  // Use webPath to display the new image instead of base64 since it's
+  // already loaded into memory
+  return {
+    filepath: fileName,
+    webviewPath: photo.webPath
+  };
+};
+  
+  return {
+  photos,
+  takePhoto
+};
+}
+
+export interface Photo {
+  filepath: string;
+  webviewPath?: string;
+}
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,25 +1,41 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+//import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
+import { camera } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+         IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
+         IonCol, IonImg } from '@ionic/react';
+
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+
 const Tab2: React.FC = () => {
+   const { photos, takePhoto } = usePhotoGallery();
+   
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
+  <IonHeader>
+    <IonToolbar>
+      <IonTitle>Brocoli</IonTitle>
+    </IonToolbar>
+  </IonHeader>
+ <IonContent>
+  <IonFab vertical="bottom" horizontal="center" slot="fixed">
+    <IonFabButton onClick={() => takePhoto()}>
+      <IonIcon icon={camera}></IonIcon>
+    </IonFabButton>
+  </IonFab>
+  <IonGrid>
+    <IonRow>
+      {photos.map((photo, index) => (
+        <IonCol size="6" key={index}>
+          <IonImg src={photo.webviewPath} />
+        </IonCol>
+      ))}
+    </IonRow>
+  </IonGrid>
+</IonContent> 
+</IonPage>
   );
 };
 
